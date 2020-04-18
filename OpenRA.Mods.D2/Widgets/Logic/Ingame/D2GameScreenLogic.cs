05af0f3b1ce1a8da7343dff13fd85b90aa92901d
alexeik <transmissive.trash@gmail.com>
2020-04-18 23:58:52 +0300

    dune2 mentat ui

---
+++ OpenRA.Mods.D2/Widgets/Logic/Ingame/D2GameScreenLogic.cs
@@ -0,0 +1,31 @@
+﻿using OpenRA.Mods.Common.Widgets;
+using OpenRA.Widgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.D2.Widgets.Logic.Ingame
+{
+    public class D2GameScreenLogic :ChromeLogic
+    {
+        bool vismentata = false;
+        BackgroundWidget mentatabkg;
+
+        [ObjectCreator.UseCtor]
+        public D2GameScreenLogic(Widget widget, World world)
+        {
+            var mainMenu = widget.Get<ButtonWidget>("mentat");
+            mainMenu.OnClick = OpenMentat;
+            mentatabkg = widget.Get<BackgroundWidget>("mentatabkg");
+            mentatabkg.IsVisible = () => vismentata;
+        }
+
+        public void OpenMentat()
+        {
+            vismentata = !vismentata;
+            mentatabkg.IsVisible = () => vismentata;
+        }
+    }
+}
