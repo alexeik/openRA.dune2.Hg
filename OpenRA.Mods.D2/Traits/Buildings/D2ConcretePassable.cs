bc7103b27501e3f455fa6ad380bf603836f5e499
alexeik <transmissive.trash@gmail.com>
2020-11-29 18:28:56 +0300

    d2 concrete passable traitinfo for pathfinding

---
+++ OpenRA.Mods.D2/Traits/Buildings/D2ConcretePassable.cs
@@ -0,0 +1,32 @@
+﻿using OpenRA;
+using OpenRA.Traits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.D2.Traits.Buildings
+{
+    public class D2ConretePassableInfo : ITraitInfo, ITemporaryBlockerInfo
+    {
+        public virtual object Create(ActorInitializer init) { return new D2ConretePassable(init.Self); }
+    }
+
+    public class D2ConretePassable : ITemporaryBlocker
+    {
+        public D2ConretePassable(Actor self) { }
+
+        bool ITemporaryBlocker.IsBlocking(Actor self, CPos cell)
+        {
+            return false;
+        }
+        bool ITemporaryBlocker.CanRemoveBlockage(Actor self, Actor blocking)
+        {
+            return true;
+        }
+    }
+
+
+
+}
